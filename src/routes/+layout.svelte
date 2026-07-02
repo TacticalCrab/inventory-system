9ac037cd1d437dfbf2929e21294874eb79c05088
Piotr <pkaleksandrowicz@gmail.com>
2026-07-02 09:29:19 +0200

    feature: navbar & navigation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import './layout.css';
+	import { resolve } from '$app/paths';
 	import favicon from '$lib/assets/favicon.svg';
 	import ToastContainer from '$lib/ui/toaster/ToastContainer.svelte';
 	import { setToastContext } from '$lib/ui/toaster/toast.svelte';
@@ -10,6 +11,19 @@
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
+
+<div class="navbar bg-base-100 shadow-sm sticky top-0 z-10">
+	<a 
+		href={resolve("/")}
+		class="btn btn-ghost text-xl">
+		Inventory System
+	</a>
+	<div class="ml-4">
+		<a href={resolve('/items')} aria-label="items">
+			Items
+		</a>
+	</div>
+</div>
 {@render children()}
 
 <ToastContainer />
